a1ce301384f63c1ecc9eb89646555f07df32128d
Daniel Hood <hood.danielc@gmail.com>
2017-08-16 21:40:33 -0700

    WIP

--- gliss/parser.h
+++ gliss/parser.h
@@ -1,5 +1,10 @@
+#pragma once
+
+#include <memory>
+
 #include "token.h"
 #include "cursor.h"
+#include "expr.h"
 
 namespace gliss {
 
@@ -8,7 +13,7 @@ class parser_t final {
 public:
 
   /* Convert an array of tokens into a syntax tree. */
-  static std::unique_ptr<expr_t> parse(const std::vector &tokens) {
+  static std::unique_ptr<expr_t> parse(const std::vector<token_t> &tokens) {
     cursor_t cursor(&tokens[0], &tokens[tokens.size() - 1]);
     return parser_t(cursor).parse();
   }
@@ -17,10 +22,10 @@ private:
 
   const cursor_t cursor;
 
-  parser_t(const cursor_t &cursor) : cursor(cursor_) {}
+  parser_t(const cursor_t &cursor_) : cursor(cursor_) {}
 
-  std::unique_ptr<expr_t> parser(const cursor_t &cursor) {
-    std::unique_ptr<expr_t> ptr;
+  std::unique_ptr<expr_t> parse() {
+    auto ptr = std::make_unique<expr_t>();
     return ptr;
   }
 
--- gliss/test/parser-test.cc
+++ gliss/test/parser-test.cc
@@ -1,9 +1,16 @@
 #include <iostream>
 #include <lick/lick.h>
+#include <gliss/lexer.h>
 #include <gliss/parser.h>
 
 using namespace gliss;
 
 FIXTURE(constructs) {
-  
+  auto tokens = lexer_t::lex(R"(
+    in vec3 vertex_position;
+
+    void main() {
+      gl_Position = vec4(vertex_position, 1.0);
+    }
+  )");
 }
