9337c8f78b95f0a663546a55cbba2c32e3cecf7f
Daniel Hood <hood.danielc@gmail.com>
2017-08-22 00:59:49 -0400

    Parse assignment operator

--- gliss/parser.h
+++ gliss/parser.h
@@ -23,22 +23,69 @@ public:
     return parser_t(cursor).parse();
   }
 
-  /* Auto matcher */
-  template <typename node_t>
+  /*
+   * #### assignment_operator:
+   *  _ADD_ASSIGN_
+   *  _AND_ASSIGN_
+   *  _DIV_ASSIGN_
+   *  _EQUAL_
+   *  _LEFT_ASSIGN_
+   *  _MOD_ASSIGN_
+   *  _MUL_ASSIGN_
+   *  _OR_ASSIGN_
+   *  _RIGHT_ASSIGN_
+   *  _SUB_ASSIGN_
+   *  _XOR_ASSIGN_
+   */
   std::unique_ptr<ast::ast_t> match_assignment_operator() {
-    // it's time
-    return std::make_unique<ast::nothing_t>();
+    auto match = cursor.match_any({
+      token_t::equal,
+      token_t::mul_assign,
+      token_t::div_assign,
+      token_t::mod_assign,
+      token_t::add_assign,
+      token_t::sub_assign,
+      token_t::left_assign,
+      token_t::right_assign,
+      token_t::and_assign,
+      token_t::xor_assign,
+      token_t::or_assign
+    });
+    switch (match->get_kind()) {
+      case token_t::mul_assign:
+        return std::make_unique<ast::assignment_operator_mul_assign_t>(std::make_unique<token_t>(*match));
+      case token_t::equal:
+        return std::make_unique<ast::assignment_operator_equal_t>(std::make_unique<token_t>(*match));
+      case token_t::div_assign:
+        return std::make_unique<ast::assignment_operator_div_assign_t>(std::make_unique<token_t>(*match));
+      case token_t::mod_assign:
+        return std::make_unique<ast::assignment_operator_mod_assign_t>(std::make_unique<token_t>(*match));
+      case token_t::add_assign:
+        return std::make_unique<ast::assignment_operator_add_assign_t>(std::make_unique<token_t>(*match));
+      case token_t::sub_assign:
+        return std::make_unique<ast::assignment_operator_sub_assign_t>(std::make_unique<token_t>(*match));
+      case token_t::left_assign:
+        return std::make_unique<ast::assignment_operator_left_assign_t>(std::make_unique<token_t>(*match));
+      case token_t::right_assign:
+        return std::make_unique<ast::assignment_operator_right_assign_t>(std::make_unique<token_t>(*match));
+      case token_t::and_assign:
+        return std::make_unique<ast::assignment_operator_and_assign_t>(std::make_unique<token_t>(*match));
+      case token_t::xor_assign:
+        return std::make_unique<ast::assignment_operator_xor_assign_t>(std::make_unique<token_t>(*match));
+      case token_t::or_assign:
+        return std::make_unique<ast::assignment_operator_or_assign_t>(std::make_unique<token_t>(*match));
+    }
   }
 
 private:
 
-  const cursor_t cursor;
+  cursor_t cursor;
 
   parser_t(const cursor_t &cursor_) : cursor(cursor_) {}
 
   std::unique_ptr<ast::ast_t> parse() {
     auto ptr = std::make_unique<ast::nothing_t>();
-    return ptr;
+    return std::move(ptr);
   }
 
 };  // parser_t
--- gliss/test/parser-test.cc
+++ gliss/test/parser-test.cc
@@ -6,5 +6,12 @@
 using namespace gliss;
 
 FIXTURE(auto_match) {
+  auto tokens = lexer_t::lex(R"(
+    =
+  )");
+
+  lexer_t::print_tokens(tokens);
+  auto parser = parser_t::make(tokens);
+  auto ast = parser.match_assignment_operator();
   // a dragon does not slay itself
 }
