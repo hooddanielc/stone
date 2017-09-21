c27bd954597279f8c5010f83b8e2999545546045
Daniel Hood <hood.danielc@gmail.com>
2017-09-21 00:26:37 -0700

    Fix bootstrap for osx build Install ib in bootstrap script Build biglr if does not exist

--- big-lalr/tools/bootstrap.sh
+++ big-lalr/tools/bootstrap.sh
@@ -3,14 +3,26 @@
 ####################################################
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+PROJECT_ROOT="$DIR/../.."
+export IB_DIR="$HOME/.ib"
 export NVM_DIR="$HOME/.nvm"
 
 if ! test -e $NVM_DIR; then
-  echo 'install nvm next to biglr'
+  echo 'installing nvm'
   export NVM_DIR="$DIR/.nvm"
 fi
 
-if ! type "nvm" > /dev/null; then
+if ! test -e $IB_DIR; then
+  IB_DIR="$DIR/.ib"
+fi
+
+if ! type "ib" &> /dev/null; then
+  echo "install ib $IB_DIR"
+  git clone https://github.com/JasonL9000/ib.git $IB_DIR
+  export PATH=$PATH:$IB_DIR
+fi
+
+if ! type "nvm" &> /dev/null; then
   # install local nvm next to biglr
   if [ ! -d "$NVM_DIR" ]; then
     git clone https://github.com/creationix/nvm.git $NVM_DIR --branch v0.33.4
@@ -27,4 +39,8 @@ if ! test -e "$DIR/dist/react-report.js" || ! test -e "$DIR/dist/server.js"; the
   npm run build
 fi
 
-node --version
+if ! test -e "$PROJECT_ROOT/../out"; then
+  # if user does not have this folder, they should build it again
+  echo 'Building biglr for first time'
+  cd "$PROJECT_ROOT" && ib --cfg osx-release big-lalr/biglr
+fi
